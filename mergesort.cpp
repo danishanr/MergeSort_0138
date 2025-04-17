b4a416c6f9d33a972abe243efe0734ef45351bc4
danishanr <danishaanr@gmail.com>
2025-04-17 15:12:25 +0700

    Membuat kode perulangan

--- mergesort.cpp
+++ mergesort.cpp
@@ -21,4 +21,11 @@ void input(){
     cout << "\n=====================";
     cout << "\nInputkan Isi Element Array";
     cout << "\n=====================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke - " << i << ": ";
+        cin >> arr[i];
+    }
+    
 }
