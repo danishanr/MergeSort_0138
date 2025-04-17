899eecdf0f0d1d80cb551906adfe17e02ffd5edf
danishanr <danishaanr@gmail.com>
2025-04-17 15:09:13 +0700

    Pembuatan prosedur

---
+++ mergesort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int arr[20], B[20];
+
+int n;
+
+void input(){
+    while (true){
+        cout << "Masukkan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nMaksimal panjang array adalah 20";
+
+        }  
+    }
+    cout << "\n=====================";
+    cout << "\nInputkan Isi Element Array";
+    cout << "\n=====================" << endl;
+}
